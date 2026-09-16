0ba3b3830b29c8fc143f268b98c6192ffd7298ec
miamargolin <miam543@nycstudents.net>
2026-09-16 08:55:05 -0400

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,28 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+
+t.forward(200)
+
+def message(input):
+    print(input)
+message("Hello Class")
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200)
+
+turtle.done()
+
+
+
+
+
