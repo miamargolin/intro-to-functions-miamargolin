b88881699c290cc561cbea5712ddb5cd37062a82
miamargolin <miam543@nycstudents.net>
2026-09-16 09:20:03 -0400

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,12 +4,13 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
 
+""" 
 def message(input):
     print(input)
-message("Hello Class")
-def square(x):
+message("Hello Class") """
+
+""" def square(x):
     t.forward(x)
     t.left(90)
     t.forward(x)
@@ -20,9 +21,39 @@ def square(x):
     t.left(90)
 square(200)
 
+def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right() """
+
+def rectangle():
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+rectangle()
+
+def triangle():
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+triangle()  
+            
 turtle.done()
-
-
-
-
 
