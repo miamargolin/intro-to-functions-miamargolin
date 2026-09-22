5067b12e8f7951fe10f44ca563e151b1bc72c6d9
miamargolin <miam543@nycstudents.net>
2026-09-22 08:45:30 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -9,21 +9,44 @@ t.shape('turtle')
     t.left(90)
 print(i)  """
 
+"""  for i in range(3):
+     t.forward(100)
+     t.left(120)
+ print(i) """
 
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+square(200) """
 
-def square(x):
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
-
-for i in range(60):
+""" for i in range(60):
     square(100)
     t.right(5)
+print(i) """
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+square(100,90)
+
+
+
+
+
+
+
+
+
+
+
 
 turtle.done()
