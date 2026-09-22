8ee7c9b019f685120d9220ee9c02b6642bf647cc
miamargolin <miam543@nycstudents.net>
2026-09-22 09:20:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,36 @@ for i in range(1,3):
 name = "Inaya"
 #input asks the user a question and records the answer
 #what we write in input argument is what user sees
-bill = input("How much was the bill")
+#input ALWAYS outputs a string
+bill = int(input("How much was the bill"))
 print(bill)
 
+if bill == 10:
+    print("Match")
+else:
+    print("no match")
 
+#integer
+amt = 100
+
+#float
+amt_two = 99.99
+
+#Boolean
+x = True
+y = False
+
+
+
+
+
+
+
+
+
+
+
+
+bill = input("how much was the bill?")
+print(int(bill + 20))
+#input is a string always
