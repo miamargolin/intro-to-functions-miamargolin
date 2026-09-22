5a6634f2ff7bcdcb47b46f89f74b174802aed0cb
miamargolin <miam543@nycstudents.net>
2026-09-22 08:45:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 #i is am incrementor/iterator
-for i in range(3):
+for i in range(1,3):
     print(i)
 
-
-
+#String for characters
+name = "Inaya"
+#input asks the user a question and records the answer
+#what we write in input argument is what user sees
+bill = input("How much was the bill")
+print(bill)
 
 
